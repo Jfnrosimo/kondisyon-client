679cf435a5564b3632b2b95d1dde1f0c26c1e68a
jrosi <j.rosimo098@gmail.com>
2022-12-06 03:56:06 +0800

    Fix: data display in pages

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -4,8 +4,16 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { fetchCurrentUser } from "../redux/reducers/usersSlice";
 
+//Import component
+import TableRow from "../components/TableRow";
+
 //Import UI
+import { Table } from "reactstrap";
+import moment from "moment";
 import "./History.css";
+import Waiting from "../assets/waiting.png";
+import Ambulance from "../assets/ambulance.png";
+import Safe from "../assets/safe.png";
 
 const History = () => {
   const dispatch = useDispatch();
@@ -18,24 +26,58 @@ const History = () => {
         phoneNumber: localStorage.getItem("phoneNumber"),
       })
       .then((result) => {
+        console.log(result);
         dispatch(fetchCurrentUser({ ...result.data }));
       });
   }, []);
-
   //Get current user details
-  const phoneNumber = state.userDetails.phoneNumber;
+  const phoneNumber = localStorage.getItem("phoneNumber");
+  console.log(phoneNumber);
   const status = state.userDetails.status;
   const statusDesc = state.userDetails.statusDesc;
+  const userHistory = state.userHistory;
 
   return (
     <div className="history-page-container">
       <div>
         <p className=" text-black">{phoneNumber}</p>
-        <p>{status}</p>
+        <span>
+          {status.toLowerCase() === "waiting for a responder" ? (
+            <img src={Waiting} alt="waiting icon" />
+          ) : status.toLowerCase() === "responder on the way" ? (
+            <img src={Ambulance} alt="ambulance logo" />
+          ) : status.toLowerCase() === "safe now" ? (
+            <img src={Safe} alt="safe check logo" />
+          ) : null}
+          {status}
+        </span>
         <p>{statusDesc}</p>
       </div>
       <div>
         <h3 className="history-table-title text-light text-center">History</h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userHistory &&
+              userHistory.map((data, index) => {
+                return (
+                  <TableRow
+                    longitude={data.locationLongitude}
+                    latitude={data.locationLatitude}
+                    date={moment(`${data.dateUpdated}`).format(
+                      "MMMM Do YYYY, h:mm:ss a"
+                    )}
+                  />
+                );
+              })}
+          </tbody>
+        </Table>
       </div>
     </div>
   );

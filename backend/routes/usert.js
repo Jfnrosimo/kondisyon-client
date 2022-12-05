5134c572d810e50ac959e4d259ecdf7bfd5b98c2
aldenderf <aldenderfc.fabro99@gmail.com>
2022-12-05 20:57:02 +0800

    feat: update usert route (backend), added NewKondisyon

--- backend/routes/usert.js
+++ backend/routes/usert.js
@@ -31,15 +31,75 @@ router.post('/checkNumber', async ( request, response )=> {
 
 
 //Register User
-router.post('/registerTemp', async( request, response) => {
-    const newUser = new User(request.body);
+router.post('/NewKodisyon', async( request, response) => {
+   
+    User.findOne({
+        phoneNumber: request.body.phoneNumber,
 
-    newUser.save().then( result => {
-        response.send({ 
-            status: 'Kodinsyon sent'
-        });
-    });
-});
+    }).then( result => {
+        console.log(result)
+
+        if (result === null) {
+            const newUser = new User({
+                ...request.body,
+                    status: 'Waiting for a Responder',
+                    statusDesc: 'Responder will call you in a few minutes!'
+            });
+            //add new user
+            newUser.save().then( result => {
+                response.send({ 
+                    status: 'Request Sent'
+                });
+            });
+            
+        }else{
+            //add to history first then update current users status and location
+            const newHistory = new History({
+                user: result._id,
+                locationLongitude: result.locationLongitude,
+                locationLatitude: result.locationLatitude
+        
+            });
+
+            newHistory.save().then( result => {
+                console.log(result.user)
+                User.updateOne(
+         
+                    { phoneNumber: request.body.phoneNumber}, 
+                    { $set: { 
+                        locationLongitude: request.body.locationLongitude,
+                        locationLatitude: request.body.locationLatitude,
+                        status: 'Waiting for a Responder',
+                        statusDesc: 'Responder will call you in a few minutes!'
+                    } })
+                .then( result => {
+                    if( result.modifiedCount === 1 ){
+                        response.send({ status: "Request sent" });
+                    }
+                });
+
+            })
+
+            
+        }
+        
+        
+    })
+
+})
+
+
+
+    // const newUser = new User(request.body);
+
+    // newUser.save().then( result => {
+    //     response.send({ 
+    //         status: 'Kodinsyon sent'
+    //     });
+    // });
+
+
+
 
 
 module.exports =  router;

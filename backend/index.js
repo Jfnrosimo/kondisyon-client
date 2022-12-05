b51ac5c1edff0f51ba32f7f68761946f958551f9
aldenderf <aldenderfc.fabro99@gmail.com>
2022-12-05 17:06:48 +0800

    feat: in backend user route

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,38 @@
 const express = require('express');
-const server = express();
 const mongoose = require('mongoose');
-const port = 8076;
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const server = express();
+const port = 8099;
+// const dotenv = require( "dotenv");
+
+// dotenv.config();
 
 
+
+//middleWare
+server.use(morgan('dev'));
+server.use( cors() );
+server.use( bodyParser.json() );
+server.use( helmet() );
+
+
+//routes
+const userRoute = require('./routes/user')
 
 // Database connection
-mongoose.connect('mongodb+srv://tricative:WW2HjUgFqXieitJU@cluster0.zjj0pac.mongodb.net/condisyondb')
+mongoose.connect('mongodb+srv://kondisyon:kondisyon@cluster0.zjj0pac.mongodb.net/kondisyondb')
+
 
 
 server.get('/', (request, response) => {
     response.send(`Welcome to the Kondisyon api`);
 })
+
+//Routes
+server.use('/api/V1/user', userRoute)
 
 
 server.listen(

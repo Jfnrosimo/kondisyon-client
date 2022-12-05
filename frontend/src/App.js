e7b976b7f4bb2277375924ff0dea91a5f8d617fd
jrosi <j.rosimo098@gmail.com>
2022-12-05 21:13:35 +0800

    Feat: routing for old numbers and new number

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 //Import pages
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
+import History from "./pages/History";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/history" element={<History />} />
       </Routes>
     </div>
   );
